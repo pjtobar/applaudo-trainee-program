126291111731f16f402224ef1fbd29f4d653e609
pablo tobar <pablo.tobar711@gmail.com>
2019-02-14 12:21:13 -0600

    changes

--- week2/timezone.rb
+++ week2/timezone.rb
@@ -1,23 +1,54 @@
 require 'time'
+name = "events.csv"
+name_valid = "valid.csv"
+name_invalid = "invalid.csv"
 
+
+#class to define colors for string
 class String
   def red;            "\033[31m#{self}\033[0m" end
   def green;          "\033[32m#{self}\033[0m" end
 end
 
-def date_valid(name,arr,arr_inv)
+def graph_file(name_valid,arr_pass,arr_error)
   begin
-    File.open(name+'.csv','w') do |file|
-      arr.each do |a|
-        file.puts a
-      end
+    file = File.open(name_valid,'a')
       file.puts
       file.puts "GRAPH"
       file.puts "||"
-      file.puts("||Valids: #{arr.length} " + " " + "▓"*arr.length)
+      file.puts("||Valids: #{arr_pass.length} " + " " + "▓"*arr_pass.length)
       file.puts "||"
-      file.puts("||Invalids: #{arr_inv.length} " + "▓"*arr_inv.length)
+      file.puts("||Invalids: #{arr_error.length} " + "▓"*arr_error.length)
       file.puts "||"
+  rescue  Exception => e
+    puts e.message
+  end
+end
+
+def graph_bash(arr_pass,arr_error)
+  begin
+    total = arr_pass.length + arr_error.length
+    percentage_valid = ((arr_pass.length.to_f/total)*100) / 2
+    percentage_invalid = ((arr_error.length.to_f/total)*100) / 2
+
+    print "\nGRAPH\n"
+    puts "------------------------------------------------------------------------"
+    print("\nValids: #{arr_pass.length} \t")
+    puts "▓".green*percentage_valid
+    print("\nInvalids: #{arr_error.length} \t")
+    puts "▓".red*percentage_invalid
+  rescue  Exception => e
+    puts "Error: #{e.message}"
+  end
+end
+
+#Method
+def date_valid(name,arr)
+  begin
+    File.open(name,'w') do |file|
+      arr.each do |a|
+        file.puts a
+      end
     end
   rescue Exception => e
     puts e.message
@@ -26,7 +57,7 @@ end
 
 def date_invalid(name,arr)
   begin
-    File.open(name+'.csv','w') do |file|
+    File.open(name,'w') do |file|
       arr.each do |a|
         file.puts a
       end
@@ -36,59 +67,32 @@ def date_invalid(name,arr)
   end
 end
 
-
-def get_date(string)
-  pattern = /,/
-  match = pattern.match(string)
-  if match
-    "#{match.post_match}"
-  else
-    "no match"
-  end
-end
-
-def get_name(string)
-  pattern = /,/
-  match = pattern.match(string)
-  if match
-    "#{match.pre_match}"
-  else
-    "no match"
-  end
-end
-
-
 begin
-  count_general = 0
+  count = 0
   arr_pass = []
   arr_error = []
 
-  File.open('events.csv','r') do |row|
+  File.open(name,'r') do |row|
     while line = row.gets()
       begin
-        count_general = count_general + 1
+        count = count + 1
         arr_line = line.split(',')
         date_origin = Time.parse(arr_line[1])
         date_est = date_origin.getlocal("-05:00")
         arr_pass << date_est
-      rescue Exception => e
-        arr_error << line.chomp + "," + "Error in line #{count_general} -#{e.message}"
-        puts "Error in line " + count_general.to_s + " " + e.message.to_s
+      rescue ArgumentError => e
+        arr_error << "#{line.chomp}, Error in line #{count} -#{e.message}"
+        puts "Error in line #{count}, #{e.message}"
       end
     end
+    arr_pass = arr_pass.sort
   end
-rescue Exception => e
-  puts "error no se encuentra el archivo #{e.message}"
+rescue Errno::ENOENT => e
+  puts "Error, #{e.message}"
 end
-arr_pass = arr_pass.sort
-
-date_valid("valids",arr_pass,arr_error)
-date_invalid("invalids",arr_error)
 
 
-print "\nGRAPH\n"
-puts "------------------------------------------------------------------------"
-print("\nValids: #{arr_pass.length} \t")
-puts "▓".green*arr_pass.length
-print("\nInvalids: #{arr_error.length} \t")
-puts "▓".red*arr_error.length
+date_valid(name_valid,arr_pass)
+date_invalid(name_invalid,arr_error)
+graph_file(name_valid,arr_pass,arr_error)
+graph_bash(arr_pass,arr_error)
