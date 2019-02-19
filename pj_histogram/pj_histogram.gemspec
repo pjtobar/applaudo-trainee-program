385f34749e3b0ec7a4a318bcb45f44a26148aaba
pablo tobar <pablo.tobar711@gmail.com>
2019-02-19 16:00:13 -0600

    gem creation

--- pj_histogram/pj_histogram.gemspec
+++ pj_histogram/pj_histogram.gemspec
@@ -1,15 +1,13 @@
-# Archivo nueva_gema/gemadesafio.gemspec
-
 Gem::Specification.new do |s|
-  s.name = "gemadesafio"
-  s.version = '0.0.1'
-  s.date = '2017-05-02'
-  s.authors = ["David Barrientos"]
-  s.email = ["david.barrientos.f@gmail.com"]
-  s.summary = "My very first"
-  s.description = "Simple Hello World"
-  s.homepage = "https://github.com/dbarrientos/gemadesafio"
-  #s.files = ["lib/mygem3.rb"]
-  # or
-  s.files = Dir["{lib}/**/*.rb", "bin/*", "LICENSE", "*.md"]
+  s.name        = 'pj_histogram'
+  s.version     = '1.0.0'
+  s.date        = '2019-02-15'
+  s.summary     = "Histogram Hash"
+  s.description = "Histogram"
+  s.authors     = ["Pablo Tobar"]
+  s.email       = 'pablo.tobar711@gmail.com'
+  s.files       = ["lib/pj_histogram.rb"]
+  s.homepage    =
+    'http://rubygems.org/gems/pj_histogram'
+  s.license       = 'MIT'
 end
--- week2/timezone.rb
+++ week2/timezone.rb
@@ -1,98 +1,92 @@
 require 'time'
-name = "events.csv"
+name = 'events.csv'
 name_valid = nil
 name_invalid = nil
 
-#Cycle that validates the entry of the names of the resulting files
+# Cycle that validates the entry of the names of the resulting files
 loop do
-  puts "Enter the name of the file for valid dates"
-  name_valid = gets.chomp + ".csv"
-  puts "Enter the name of the file for invalid dates"
-  name_invalid = gets.chomp + ".csv"
+  puts 'Enter the name of the file for valid dates'
+  name_valid = gets.chomp + '.csv'
+  puts 'Enter the name of the file for invalid dates'
+  name_invalid = gets.chomp + '.csv'
   if name_valid != name_invalid
     break
   else
-    puts "ERROR, Enter a different name for each file"
+    puts 'ERROR, Enter a different name for each file'
   end
 end
-
 
 # Class to define colors for string
 class String
-  def red;            "\033[31m#{self}\033[0m" end
-  def green;          "\033[32m#{self}\033[0m" end
+  def red
+    "\033[31m#{self}\033[0m"
+  end
+
+  def green
+    "\033[32m#{self}\033[0m"
+  end
 end
 
 # Method to generate and save graphics in the file
-def graph_file(name_valid,arr_pass,arr_error)
-  begin
-    total = arr_pass.length + arr_error.length
-    percentage_valid = ((arr_pass.length.to_f/total)*100).round(2)
-    percentage_invalid = ((arr_error.length.to_f/total)*100).round(2)
-    file = File.open(name_valid,'a')
-      file.puts
-      file.puts "GRAPH"
-      file.puts "╔═══════════════════════════════════════════════════════════════════"
-      file.puts "║Valids: #{arr_pass.length}   #{"█"*(percentage_valid / 2)} #{percentage_valid}%"
-      file.puts "║"
-      file.puts "║Invalids: #{arr_error.length} #{"▓"*(percentage_invalid / 2)} #{percentage_invalid}%"
-      file.puts "║           % ───│────│────│────│────│────│────│────│────│────│"
-      file.puts "║               10   20   30   40   50   60   70   80   90   100"
-      file.puts "╚═══════════════════════════════════════════════════════════════════"
-  rescue  Exception => e
-    puts "ERROR, #{e.message}"
-  end
+def graph_file(name_valid, arr_pass, arr_error)
+  total = arr_pass.length + arr_error.length
+  percentage_valid = ((arr_pass.length.to_f / total) * 100).round(2)
+  percentage_invalid = ((arr_error.length.to_f / total) * 100).round(2)
+  file = File.open(name_valid, 'a')
+  file.puts
+  file.puts 'GRAPH'
+  file.puts '╔═══════════════════════════════════════════════════════════════════'
+  file.puts "║Valids: #{arr_pass.length}   #{'█' * (percentage_valid / 2)} #{percentage_valid}%"
+  file.puts '║'
+  file.puts "║Invalids: #{arr_error.length} #{'▓' * (percentage_invalid / 2)} #{percentage_invalid}%"
+  file.puts '║           % ───│────│────│────│────│────│────│────│────│────│'
+  file.puts '║               10   20   30   40   50   60   70   80   90   100'
+  file.puts '╚═══════════════════════════════════════════════════════════════════'
+rescue  Exception => e
+  puts "ERROR, #{e.message}"
 end
 
 # Method to generate the graph of results in bash
-def graph_bash(arr_pass,arr_error)
-  begin
-    total = arr_pass.length + arr_error.length
-    percentage_valid = ((arr_pass.length.to_f/total)*100).round(2)
-    percentage_invalid = ((arr_error.length.to_f/total)*100).round(2)
+def graph_bash(arr_pass, arr_error)
+  total = arr_pass.length + arr_error.length
+  percentage_valid = ((arr_pass.length.to_f / total) * 100).round(2)
+  percentage_invalid = ((arr_error.length.to_f / total) * 100).round(2)
 
-    print "\nGRAPH\n"
-    puts "╔═══════════════════════════════════════════════════════════════════"
-    print("║\n║Valids: #{arr_pass.length} \t")
-    puts "█".green*(percentage_valid / 2) + "#{percentage_valid}%"
-    print("║\n║Invalids: #{arr_error.length} \t")
-    puts "█".red*(percentage_invalid / 2) + "#{percentage_invalid}%"
-    puts "║"
-    puts "║             % ───│────│────│────│────│────│────│────│────│────│"
-    puts "║                  10   20   30   40   50   60   70   80   90   100"
-    puts "╚═══════════════════════════════════════════════════════════════════"
-
-  rescue  Exception => e
-    puts "Error: #{e.message}"
-  end
+  print "\nGRAPH\n"
+  puts '╔═══════════════════════════════════════════════════════════════════'
+  print("║\n║Valids: #{arr_pass.length} \t")
+  puts '█'.green * (percentage_valid / 2) + "#{percentage_valid}%"
+  print("║\n║Invalids: #{arr_error.length} \t")
+  puts '█'.red * (percentage_invalid / 2) + "#{percentage_invalid}%"
+  puts '║'
+  puts '║             % ───│────│────│────│────│────│────│────│────│────│'
+  puts '║                  10   20   30   40   50   60   70   80   90   100'
+  puts '╚═══════════════════════════════════════════════════════════════════'
+rescue  Exception => e
+  puts "Error: #{e.message}"
 end
 
 # Method that receives an array of valid dates to generate a file with the results.
-def date_valid(name,arr)
-  begin
-    File.open(name,'w') do |file|
-      arr.each do |a|
-        file.puts a
-      end
+def date_valid(name, arr)
+  File.open(name, 'w') do |file|
+    arr.each do |a|
+      file.puts a
     end
-  rescue Exception => e
-    puts "ERROR, #{e.message}"
   end
+rescue Exception => e
+  puts "ERROR, #{e.message}"
 end
 
 # Method that receives an array of invalid dates to generate a file with the results.
-def date_invalid(name,arr)
-  begin
-    File.open(name,'w') do |file|
-      arr.each do |a|
-        file.puts a
-      end
+def date_invalid(name, arr)
+  File.open(name, 'w') do |file|
+    arr.each do |a|
+      file.puts a
     end
-  rescue Exception => e
-    puts "ERROR, #{e.message}"
   end
+rescue Exception => e
+  puts "ERROR, #{e.message}"
 end
-
 
 # Process for validating dates
 begin
@@ -100,13 +94,13 @@ begin
   arr_pass = []
   arr_error = []
 
-  File.open(name,'r') do |row|
-    while line = row.gets()
+  File.open(name, 'r') do |row|
+    while line = row.gets
       begin
-        count = count + 1
+        count += 1
         arr_line = line.split(',')
         date_origin = Time.parse(arr_line[1])
-        date_est = date_origin.getlocal("-05:00")
+        date_est = date_origin.getlocal('-05:00')
         arr_pass << date_est
       rescue ArgumentError => e
         arr_error << "#{line.chomp}, Error in line #{count} -#{e.message}"
@@ -119,8 +113,7 @@ rescue Errno::ENOENT => e
   puts "ERROR, #{e.message}"
 end
 
-
-date_valid(name_valid,arr_pass)
-date_invalid(name_invalid,arr_error)
-graph_file(name_valid,arr_pass,arr_error)
-graph_bash(arr_pass,arr_error)
+date_valid(name_valid, arr_pass)
+date_invalid(name_invalid, arr_error)
+graph_file(name_valid, arr_pass, arr_error)
+graph_bash(arr_pass, arr_error)
