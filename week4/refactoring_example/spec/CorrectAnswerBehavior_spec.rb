aa4bf61f2c6285cb55071b7696bc6e3effa5a9a1
pjtobar <pablo.tobar711@gmail.com>
2019-03-01 09:34:07 -0600

    test when is getting out of the penalty box

--- week4/refactoring_example/spec/CorrectAnswerBehavior_spec.rb
+++ week4/refactoring_example/spec/CorrectAnswerBehavior_spec.rb
@@ -11,18 +11,26 @@ describe "CorrectAnswerBehavior" do
       expect(CorrectAnswerBehavior.new.was_correctly_answered).to eq(true)
     end
 
-    it "if is in penalty box purses must no increase" do
+    it "if is not is_getting_out_of_penalty_box purses must not increase" do
       game =  CorrectAnswerBehavior.new
-      purses = game.instance_variable_get(:@purses)[instance_variable_get(:@current_player)]
-      # puts "afteeeeeeeeeeeeeee #{purses}"
-      array = game.instance_variable_get(:@in_penalty_box)
-      index = game.instance_variable_get(:@current_player)
-      array[index] = true
-      game.instance_variable_set(:@in_penalty_box, array)
-      game.instance_variable_set(:@is_getting_out_of_penalty_box, false)
+      current_player = game.instance_variable_get(:@current_player)
+      purses = game.instance_variable_get(:@purses)[current_player]
+      value = false
+      is_in_penalty(game, value)
       game.was_correctly_answered
       # puts "despues #{game.instance_variable_get(:@purses)}"
-      expect(game.instance_variable_get(:@purses)).to eq(purses)
+      expect(game.instance_variable_get(:@purses)[current_player]).to eq(purses)
+    end
+
+    it "if is_getting_out_of_penalty_box purses must increase by 1" do
+      game =  CorrectAnswerBehavior.new
+      current_player = game.instance_variable_get(:@current_player)
+      purses = game.instance_variable_get(:@purses)[current_player]
+      value = true
+      is_in_penalty(game, value)
+      game.was_correctly_answered
+      # puts "despues #{game.instance_variable_get(:@purses)}"
+      expect(game.instance_variable_get(:@purses)[current_player]).to eq(purses + 1)
     end
   end
   describe "#check_current_player" do
@@ -32,13 +40,13 @@ describe "CorrectAnswerBehavior" do
       expect(CorrectAnswerBehavior.new.was_correctly_answered).to eq(true)
     end
 
-    it "current_player must change to zero when current_player" do
+    it "current_player must change to zero when is the last one" do
       game = CorrectAnswerBehavior.new
       game.instance_variable_set(:@current_player, game.instance_variable_get(:@players).length-1)
       expect(game.check_current_player).to eq(0)
     end
 
-    it "current_player must increase by 1" do
+    it "current_player must increase by 1 when not is the last one" do
       game = CorrectAnswerBehavior.new
       game.instance_variable_set(:@current_player, rand(game.instance_variable_get(:@players).count - 1))
       player = game.instance_variable_get(:@current_player)
@@ -58,4 +66,12 @@ describe "CorrectAnswerBehavior" do
       expect(game.check_correct_answer).to eq(purses + 1)
     end
   end
+end
+
+def is_in_penalty (game, value)
+  array = game.instance_variable_get(:@in_penalty_box)
+  index = game.instance_variable_get(:@current_player)
+  array[index] = true
+  game.instance_variable_set(:@in_penalty_box, array)
+  game.instance_variable_set(:@is_getting_out_of_penalty_box, value)
 end
